3de1bf86cdc233728e9b8fa00dee4397702e0578
LucasPapini <papine12345@hotmail.com>
2025-06-03 07:39:18 -0300

    Criando um teste de integracao para validar a aplicao do CORS

--- src/test/java/br/com/erudio/config/TestConfigs.java
+++ src/test/java/br/com/erudio/config/TestConfigs.java
@@ -2,4 +2,9 @@ package br.com.erudio.config;
 
 public interface TestConfigs {
     int SERVER_PORT = 8888;
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_ERUDIO = "http://localhost:8080";
+    String ORIGIN_SEMERU = "http://localhost:3000";
 }
